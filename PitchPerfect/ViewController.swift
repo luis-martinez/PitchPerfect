06812fdfa6666e4af4341eea293aa4e1861b1fba
Luis Martínez <_>
2016-04-23 12:26:56 -0700

    Added buttons and images

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        print("Record button pressed")
+        recordingLabel.text = "Recording in progress..."
+        stopRecordingButton.enabled = true
+        recordButton.enabled = false
+    }
 
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("Stop recording button pressed")
+        recordButton.enabled = true
+        stopRecordingButton.enabled = false
+        recordingLabel.text = "Tap to Record"
+    }
+    
 }
 
