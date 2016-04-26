d9b307aac80b91b19b2ce018b5f3e65317a3e911
Luis Martínez <_>
2016-04-25 20:55:52 -0700

    Added icon, other images, buttons, external file, ...

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,15 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
-    var recordedAudioURL: NSURL!
 
+    @IBOutlet weak var SnailButton: UIButton!
+    @IBOutlet weak var RabbitButton: UIButton!
+    @IBOutlet weak var ChipmunkButton: UIButton!
+    @IBOutlet weak var DarthVaderButton: UIButton!
+    @IBOutlet weak var EchoButton: UIButton!
+    @IBOutlet weak var ReverbButton: UIButton!
+    @IBOutlet weak var StopButton: UIButton!
+    
+    var recordedAudioURL: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
+    
+    enum ButtonType: Int { case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb }
+    
+
+    @IBAction func playSoundForButton(sender: UIButton){
+        print("Play sound button pressed")
+        switch (ButtonType(rawValue: sender.tag)!) {
+        case .Slow:
+            playSound(rate: 0.5)
+        case .Fast:
+            playSound(rate: 1.5)
+        case .Chipmunk:
+            playSound(pitch: 1000)
+        case .Vader:
+            playSound(pitch: -1000)
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        }
+        configureUI(.Playing)
+    }
+    
+    @IBAction func stopButtonPressed(sender: AnyObject){
+        print("Stop audio button pressed")
+        stopAudio()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("PlaySoundsVideoController loader")
+        setupAudio()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        configureUI(.NotPlaying)
     }
 
     override func didReceiveMemoryWarning() {
